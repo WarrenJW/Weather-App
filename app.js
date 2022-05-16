ba543446a174aec6752dd451b13949edaa7eb68e
WarrenJW <wjwnetsolutions@gmail.com>
2022-05-16 18:41:55 +0100

    app.js

--- app.js
+++ app.js
@@ -2,14 +2,29 @@ window.addEventListener("load", () => {
     let long;
     let lat;
 
-    if(navigator.geolocation){
+    if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
             long = position.coords.longitude;
             lat = position.coords.latitude;
-            console.log();
 
-        });
+            const proxy ="https:/cors-anywhere.herokuapp.com";
+            const api = `http://api.weatherapi.com/v1/current.json?key=58a408c65940441daab164353221605&q=London&aqi=no`;
+
+            fetch(api)
+                .then(response =>  {
+                    return response.json();
+                })
+                .then(data => {
+                    console.log(data);
+                    const { condition, feelslike_c } = data.current;
+                });
+         });
     }
 });
+
+
+
+
+
 
 
